125488b7ccb846967b4553135c2e13c20ba1370d
abdullaeyv06 <145132841+abdullaeyv06@users.noreply.github.com>
2023-11-17 02:43:28 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+let ask = prompt("what is your name") .toUpperCase()
+
+if (ask[0].toUpperCase() == "A") {
+     let age = prompt("how old are you")
+    if (age >=20 && age <40  ) {
+        let money = prompt("how much do you have")
+        if (money >= 100) {
+          let   ask_num = prompt("how many are you")
+        }else {
+            alert("error")
+        }
+        
+    }else {
+        alert("error")
+    }
+    
+} else {
+    
+    alert("error")
+}
